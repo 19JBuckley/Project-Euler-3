ca5c9716ec7418d49fa11156a507733c7d1c0591
Joshua Buckley <19JBuckley@heckgrammar.co.uk>
2024-10-17 15:09:28 +0100

    Add project files.

---
+++ Project Euler 3/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace ProjectEuler_3
+{
+    class Program
+    {
+        static long input = 600851475143;
+
+        static void Main()
+        {
+            long lastPrimeFactor = 0;
+
+            for (long i = 2; i < input; i++)
+            {
+                if (primeBool(i) && input % i == 0)
+                {
+                    lastPrimeFactor = i;
+                }
+            }
+            Console.WriteLine(lastPrimeFactor);
+        }
+
+        static bool primeBool(long n)
+        {
+            if (n < 4) return n > 1;
+            if (n % 2 == 0 || n % 3 == 0) return false;
+
+            long limit = (long)Math.Sqrt(n);
+            for (long i = 5; i <= limit; i += 2)
+            {
+                if (n % i == 0) return false;
+            }
+
+            return true;
+        }
+    }
+}
